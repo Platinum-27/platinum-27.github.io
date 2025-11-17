88279b297e44a1fff649a39d013a854c7b47ad36
Ilyas Bennouri <escrimeur10@gmail.com>
2025-11-16 23:56:30 -0500

    Price button added

--- navLoader.js
+++ navLoader.js
@@ -85,3 +85,21 @@ function addAnimation() {
   });
 }
 
+/*---Pricing button---*/
+const checkbox = document.querySelector('#checkbox');
+const label = document.querySelector('.label');
+
+checkbox.addEventListener('change', showPlan);
+
+function showPlan() {
+  label.classList.toggle('selected')
+  displayPrice();
+}
+
+function displayPrice() {
+  const prices = document.querySelectorAll(".prices");
+
+  prices.forEach(item => {
+    item.classList.toggle('show_price')
+  })
+}
